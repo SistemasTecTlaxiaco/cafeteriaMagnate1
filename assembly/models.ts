8c554bb3fba53e45a7db35e39855c0b047e12c84
Magaly Nicolas Sanchez <71893476+magaly1311@users.noreply.github.com>
2023-05-28 21:27:55 -0600

    diagrama de clases
    productos y clientes

--- assembly/models.ts
+++ assembly/models.ts
@@ -1,36 +1,53 @@
 import { context, PersistentVector, u128 } from "near-sdk-as";
 @nearBindgen
-export class Book {
-  name: string;
-  description: string;
-  owner: string;
-  image: string;
-  file: string;
-  author: string;
-  language: string;
-  publisher: string;
+export class productos {
+  id_productos: string;
+  nombre: string;
+  descripcion: string;
+  existencias: string;
+  caducidad: string;
+  marca: string;
+  precio: string;
 
-  constructor(name: string, description: string,image: string, file: string, author: string, language: string, publisher: string) {
-    this.owner = context.sender;
-    this.name = name;
-    this.description = description;
-    this.image = image;
-    this.file = file;
-    this.author = author;
-    this.language = language;
-    this.publisher = publisher;
+  ventas: string;
+
+  constructor(id_productos: string, nombre: string,decripcion: string, existencias: string, caducidad: string, marca: string, ventas: string) {
+    this.id_productos = id_productos;
+    this.nombre = nombre;
+    this.descripcion = this.descripcion;
+    this.existencias = existencias;
+    this.caducidad = caducidad;
+    this.marca = marca;
+    this.precio = this.precio;
+
+    this.ventas = ventas;
   }
 }
 
 @nearBindgen
-export class Contributor {
-    user: string;
-    constructor() {
-        this.user = context.sender;
+export class cliente {
+  id_cliente:string;
+  nombre:string;
+  apellidos:string;
+  cuenta:string;
+  direccion:string;
+  telefono:string;
+
+  wallet:string;
+
+    constructor(id_clintes:string,nombre:string,apellidos:string,cuenta:string,direccion:string,telefono:string,
+      wallet:string) {
+        this.id_cliente =id_clintes;
+        this.nombre=nombre;
+        this.apellidos=apellidos;
+        this.cuenta=cuenta;
+        this.direccion=direccion;
+        this.telefono=telefono;
+
+        this.wallet=wallet;
     }
 }
 
-export const allBooks = new PersistentVector<Book>("v")
-export const allContributors = new PersistentVector<Contributor>("c")
+export const allproductos = new PersistentVector<productos>("productos")
+export const allcliente = new PersistentVector<cliente>("cliente")
 export const ONE_NEAR = u128.from('10000000000000000')
-
