c2dd206bc3567d271f57187bc0ae8b1ad21bcd4e
Magaly Nicolas Sanchez <71893476+magaly1311@users.noreply.github.com>
2023-05-29 22:27:21 -0600

    index modi

--- assembly/__tests__/index.spec.ts
+++ assembly/__tests__/index.spec.ts
@@ -1,59 +1,65 @@
-import {Book, allBooks, allContributors, Contributor} from "../models"
-import { getBooks, uploadBook, deleteBooks, booksLen, getContributors, findContributor, deleteContributors, getContributorsLength, deleteBook } from "..";
+import {cliente, allclientes, allproductos, productos} from "../models"
+import { Registrar_productos,Buscar_productos, Actualizar_productos, Eliminar_productos,Registrar_cliente,
+Buscar_cliente, Actualizar_clientes, Eliminar_cliente, Identificar_cliente,Cancelar_producto } from "..";
 import { context, Context, logging } from "near-sdk-as";
 
+const id_productos='producto'
+const nombre = 'Rust Learning Book'
+const descripcion = 'Book written for all those people interested in learning the powerfull and reliable Rust Programming Language.'
+const existencias ='https://ipfs.io/ipfs/bafybeiet7srgvvxkzwrcaayorzmecmirkizmbinwwcn2k7wxpaszvfmzdi'
+const caducidad = 'https://ipfs.io/ipfs/bafybeia5khhhukn672acm6sfredqdereor7n7zsoobvrwcqk7rmn6ihffi'
+const marca = 'Book Author'
+const precio = 'Book Publisher'
+const ventas = 'English'
 
-const name = 'Rust Learning Book'
-const description = 'Book written for all those people interested in learning the powerfull and reliable Rust Programming Language.'
-const image ='https://ipfs.io/ipfs/bafybeiet7srgvvxkzwrcaayorzmecmirkizmbinwwcn2k7wxpaszvfmzdi'
-const file = 'https://ipfs.io/ipfs/bafybeia5khhhukn672acm6sfredqdereor7n7zsoobvrwcqk7rmn6ihffi'
-const author = 'Book Author'
-const publisher = 'Book Publisher'
-const language = 'English'
-
-const contributorUser = 'Bob'
+const nuevocliente = 'Bob'
 
 
-let newBook = new Book(name, description, image, file, author, language, publisher);
+let nuevoproductos = new productos(id_productos, nombre, descripcion, existencias,caducidad, marca,precio, ventas);
 
-const allBooksIndex = allBooks.length;
-const allContributorsIndex = allContributors.length;
+const allproductosIndex = allproductos.length;
+const allclienteIndex = allclientes.length;
 
-const contsData = new Array<Contributor>(allContributorsIndex);
-for(let x = 0; x < allContributorsIndex; x++) {
-    contsData[x] = allContributors[x]
+const contsData = new Array<cliente>(allclienteIndex);
+for(let x = 0; x < allclienteIndex; x++) {
+    contsData[x] = allclientes[x]
 }
 
-const data = new Array<Book>(allBooksIndex);
-for(let i=0; i < allBooksIndex; i++) {
-    data[i] = allBooks[i]
+const data = new Array<productos>(allproductosIndex);
+for(let i=0; i < allproductosIndex; i++) {
+    data[i] = allproductos[i]
 }
 
 describe("uploadBook", () => {
     it('should return "newBookUpload"', () => {
-        expect(uploadBook('Rust Learning Book', 'Book written for all those people interested in learning the powerfull and reliable Rust Programming Language.','https://ipfs.io/ipfs/bafybeiet7srgvvxkzwrcaayorzmecmirkizmbinwwcn2k7wxpaszvfmzdi', 'https://ipfs.io/ipfs/bafybeia5khhhukn672acm6sfredqdereor7n7zsoobvrwcqk7rmn6ihffi', 'Book Author', 'English', 'Book Publisher')).toStrictEqual(newBook);
+        expect(Registrar_productos('id_productos: string, nombre: string, descripcion: string, existencias: string, caducidad: string, marca: string, precio: string, ventas: string')).toStrictEqual(nuevoproductos);
     })
 })
 
 describe("getBooks", () => {
     it('should return all books', () => {
-        expect(getBooks()).toStrictEqual(data)
+        expect(Buscar_productos()).toStrictEqual(data)
     })
 })
 
 describe("getContributors", () => {
     it('should return all contributors', () => {
-        expect(getContributors()).toStrictEqual(contsData)
+        expect(Actualizar_productos()).toStrictEqual(nuevoproductos)
     })
 })
 
 describe("findContributor", () => {
 
     it('should be true', () => {
-        expect(findContributor(contributorUser)).toBeFalsy()
+        expect(findContributo(contributorUser)).toBeFalsy()
     })
 })
+describe("findContributor", () => {
 
+    it('should be true', () => {
+        expect(findContributo(contributorUser)).toBeFalsy()
+    })
+})
 describe("deleteContributors", () => {
     it("should delete all the contributors", () => {
         deleteContributors()
